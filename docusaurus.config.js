2508ae5491cb49e3b35cbbe070fa396bc5b78f3f
Volumio <info@volumio.org>
2021-10-18 18:23:31 +0200

    Fix broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
               },
               {
                 label: 'User Manual',
-                to: 'manual.volumio.org',
+                to: 'https://manual.volumio.org',
               },
             ],
           },
